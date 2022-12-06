eb28bed613a8235290c0563613fb34257648cc1d
RotrixxLOL <ncusnir43@gmail.com>
2022-12-06 12:13:04 +0100

    chore: add extra

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Routes, Route } from "react-router-dom";
 
 import { Footer, Home, Navbar, NewMeme } from "./components";
